741f9d72f8e6001a87e5f9a916d75762f1cdb1db
Raviteja Giduturi <raviteja_giduturi@epam.com>
2020-05-13 22:57:22 +0530

    add saparate router js file - code cleanup

--- app.js
+++ app.js
@@ -1,60 +1,7 @@
 const http = require('http');
 
-const fs = require('fs');
-
-const server = http.createServer((req, res) => {
-    // console.log(req.url, req.headers);
-
-
-    
-    if (req.url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>')
-        res.write('        <head>');
-        res.write('            <title> Node Js </title>');
-        res.write('        </head>');
-        res.write('        <body>');
-        res.write(`           
-            <form action="/message" method="POST">
-            <input type="text" name="message"/> 
-            <button type=""submit> Send ... </button></form>
-        `);
-        res.write('        </body>');
-        res.write('    </html>');
-        return res.end();
-    }
-
-    if (req.url === '/message' && req.method == "POST") {
-        const body = [];
-        req.on('data', (chunk)=>{
-            console.log('chunk ' + chunk);
-            body.push(chunk);
-        });
-        req.on('end', (chunk)=>{
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(Buffer.concat(body));
-            console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        })
-
-        res.statusCode = 302; // Redirect found.
-        res.setHeader('Location','/'); // new location path.
-        return res.end();
-    }
-
-    res.write('<html>')
-    res.write('        <head>');
-    res.write('            <title> Node Js </title>');
-    res.write('        </head>');
-    res.write('        <body>');
-    res.write('            Welcome, Hello Worldl!!!!!!!!!!!!!');
-    res.write('        </body>');
-    res.write('    </html>');
-    return res.end();
-
-    //  process.exit(1);
-})
+const routers = require('./routers');
+const server = http.createServer(routers);
 
 server.listen(3000, '127.0.0.1', () => {
     console.log(`Server Runnig ...`);
